5826dcd21faa30d32be4c946d4f84bfd45adfc18
Matthew P. Parker <matthew.p.parker.20@dartmouth.edu>
2020-05-20 10:56:04 -0400

    initial structure of CNode_helper

---
+++ src/p2p/CNode_helper.py
@@ -0,0 +1,111 @@
+"""
+Child Node Helper files for COSC 60 FInal Project
+
+
+May 2020
+"""
+
+import socket
+import binascii
+from mrt import * 
+
+""" DEFINITIONS """
+POST = '0001'
+REQUEST = '0101'
+FILE_TRANSFER = '1111'
+ERROR = '0000'
+
+""" SUB DEFINITIONS """
+
+""" REQUESTS """
+R_JOIN = '000a'
+R_LIST = '000b'
+R_LOCAL_DHT = '000c'
+R_ALL_DHT = '000d'
+R_FILE_TRANS = '000e'
+
+
+""" POSTS """
+P_NEW_FILE = '000a'
+P_DISCONNECT = '000b'
+
+SUPERNODE_IP = ''
+SUPERNODE_PORT = 0
+SUPERNODE_ID = 0
+
+
+#TODO: GET SOURCE IP USING STUN STUFF
+SOURCE_IP = ''
+
+"""
+GENERAL NOTE: I JUST REALIZED THAT I DIDNT ADD LENGTH,
+SOURCE IPV4 ETC
+ALSO I THINK THAT WE NEED TO HAVE SOURCE PORT IN THERE 
+AS WELL 
+"""
+
+"""
+Connects to supernode using mrt_connect
+Returns an ID connection object
+"""
+def connect_p2p(ip = SUPERNODE_IP,port = SUPERNODE_PORT):
+	global SUPERNODE_ID
+	SUPERNODE_ID = mrt_connect(SUPERNODE_IP,SUPERNODE_PORT)
+
+"""
+Function to join the network, takes one parameter
+join_type:
+	0: join as chlid node
+	1: join as supernode
+	2: join as relayed supernode
+"""
+def join_p2p(join_type = 0):
+	#TODO FIX THIS TO INCLUDE LENGTH AND IPV4
+	pre_msg = ''.join([REQUEST,R_JOIN,f'{join_type:04d}'])
+	send_msg = binascii.unhexlify(pre_msg)
+	send_p2p_msg(send_msg)
+
+
+"""
+Function to create the request for DHT
+Takes one arg: all (bool)
+	all = True: send '000d'
+	all = False: send '000c'
+	default is True
+"""
+def request_dht(all_dht=True)
+	if all_dht:
+		pre_msg = ''.join([REQUEST,R_ALL_DHT])
+	else: 
+		pre_msg = ''.join([REQUEST,R_LOCAL_DHT])
+	send_msg = binascii.unhexlify(pre_msg)
+	send_p2p_msg(send_msg)
+
+"""
+Function to request supernode list
+Takes no parameters
+"""
+def request_super_list():
+	pre_msg = ''.join([REQUEST,R_LIST])
+	send_msg = binascii.unhexlify(pre_msg)
+	send_p2p_msg(send_msg)
+
+
+"""
+Function to send new file post
+PARAMETERS: 
+	File Size: int of byte size
+	File ID length: int of byte size
+	File ID: string
+"""
+def post_file(file_size,id_size,filename)
+	pre_msg = ''.join([])
+
+"""
+Function to send the pre-made p2p message. 
+Message must be in binary (use binascii.unhexlify)
+Sends to SUPERNODE_ID
+"""
+def send_p2p_msg(msg):
+	mrt_send1(SUPERNODE_ID,msg)
+
